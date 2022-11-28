d0f03df85924e5222edc4ffc1e2bec4ddd2fe291
BlandineBrnt <blandine.breant@gmail.com>
2022-11-28 14:24:44 +0100

    3

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Apropos from "./pages/Apropos";
+import Error404 from "./pages/Erreur404";
+import FicheLogement from "./pages/FicheLogement";
+import { GlobalStyle } from "./styles/style";
+import datas from "./assets/data/data.json";
+import Home from "./pages/Home";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <GlobalStyle />
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/apropos" element={<Apropos />} />
+                <Route
+                    path="/fichelogement/:id"
+                    data={datas}
+                    element={<FicheLogement />}
+                />
+                <Route path="*" element={<Error404 />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 export default App;
